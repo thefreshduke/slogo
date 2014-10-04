f5be88fa0c1f9319b993894fc456ee207127048b
Duke Kim <duke.dukkyeom.kim@gmail.com>
2014-10-04 19:59:54 -0400

    Added controller test

---
+++ src/ControllerTest.java
@@ -0,0 +1,25 @@
+import javafx.scene.Node;
+import org.junit.Test;
+
+
+public class ControllerTest {
+    
+    @Test
+    public void testThatCommandIsReceived(){}
+    
+    @Test
+    public void testThatPauseOccurs(){}
+    
+    @Test
+    public void testThatModelInitializes(){}
+    
+    @Test
+    public void testThatErrorPropagates(){}
+    
+    @Test
+    public void testThatHardSetOverridesState(){}
+    
+    @Test
+    public void testThatHardSetClearsHistory(){}
+
+}
