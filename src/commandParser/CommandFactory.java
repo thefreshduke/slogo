9a367c627e18858c02adb4bcf8ef1529ba546cc6
Rahul Harikrishnan <rh151@duke.edu>
2014-10-07 19:38:35 -0400

    Created new class for parsing the language resource file

--- src/commandParser/CommandFactory.java
+++ src/commandParser/CommandFactory.java
@@ -1,85 +1,42 @@
 package commandParser;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 import java.util.Map;
-
-import backendExceptions.BackendException;
-import backendExceptions.ExceptionMessages;
-import backendExceptions.SlogoFileNotFoundException;
 import commands.BaseCommand;
 
 public class CommandFactory {
-    
-    private static Map<String, Class<BaseCommand>> myCommandToClassMap;
-    private static String myCommandSeparator;
-    private String myEquals;
-    private String myComma;
-    
-    protected void parseLanguageFile(String fileName) throws BackendException {
-    	BufferedReader reader = null;
+
+	private static Map<String, Class<BaseCommand>> myCommandToClassMap;
+	private static String myCommandSeparator;
+
+	//TODO: Need to figure out how to call the parseLanguageFile method in LanguageFileParser class
+	// only once.  This populates the myCommandToClassMap object.
+
+	public static BaseCommand createCommand(String input){
+		String firstCommand = identifyFirstCommand(input);
+		Class<BaseCommand> commandClass = myCommandToClassMap.get(firstCommand);
+		if(commandClass == null){
+			//TODO: throw something
+		}
+		String subInput = input.replaceFirst(firstCommand + myCommandSeparator, "");
+		BaseCommand command = null;
 		try {
-			reader = new BufferedReader(new FileReader(fileName));
-		} catch (FileNotFoundException e1) {
-			throw new SlogoFileNotFoundException(e1, fileName);
+			command = (BaseCommand) commandClass.getConstructor(String.class).newInstance(subInput);
 		}
-    	String line;
-    	try {
-			while((line = reader.readLine()) != null) {
-				String spaceRemovedLine = line.replace("\\s+", "");
-				String[] splitLine = spaceRemovedLine.split(myEquals, 2);
-				String className = splitLine[0];
-				String[] possibleInputs = splitLine[1].split(myComma);
-				Class commandClass = Class.forName(className);
-				boolean extendsBaseCommand = BaseCommand.class.isAssignableFrom(commandClass);
-				if(extendsBaseCommand){
-					for(String possibleInput : possibleInputs){
-						myCommandToClassMap.put(possibleInput, commandClass);
-					}
-				}
-				else{
-					//TODO: Throw exception;
-				}
-			}
-		} catch (ClassNotFoundException e) {
-			// TODO: Make Exception 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		catch(Exception ex){
+			//TODO: Change to more specific exceptions;
 		}
-    }
-    
-    public static BaseCommand createCommand(String input){
-        String firstCommand = identifyFirstCommand(input);
-        Class<BaseCommand> commandClass = myCommandToClassMap.get(firstCommand);
-        if(commandClass == null){
-            //throw something
-        }
-        String subInput = input.replaceFirst(firstCommand + myCommandSeparator, "");
-        BaseCommand command = null;
-        try {
-           command = (BaseCommand) commandClass.getConstructor(String.class).newInstance(subInput);
-        }
-        catch(Exception ex){
-        	//TODO: Change to more specific exceptions;
-        }
-        //TODO: Throw exception
-        return command;
-    }
-    
-    //TODO: remove if unnecessary
-    private void handleError(Exception ex){
-    	
-    }
-    
-    private static String identifyFirstCommand(String input){
-        String[] commandList = input.split(myCommandSeparator);
-        String firstCommand = commandList[0];
-        return firstCommand;
-    }
+		//TODO: Throw exception
+		return command;
+	}
+
+	//TODO: remove if unnecessary
+	private void handleError(Exception ex){
+
+	}
+
+	private static String identifyFirstCommand(String input){
+		String[] commandList = input.split(myCommandSeparator);
+		String firstCommand = commandList[0];
+		return firstCommand;
+	}
 }
