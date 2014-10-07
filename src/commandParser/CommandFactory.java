d05c63cdeb1c9d7a235e8c00a3b2134b882c2e88
Duke Kim <Duke@Dukes-MacBook-Air.local>
2014-10-07 18:24:44 -0400

    Added property file parsing

--- src/commandParser/CommandFactory.java
+++ src/commandParser/CommandFactory.java
@@ -1,53 +1,78 @@
 package commandParser;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Map;
+
 import commands.BaseCommand;
 
 public class CommandFactory {
     
     private static Map<String, Class<BaseCommand>> myCommandToClassMap;
     private static String myCommandSeparator;
+    private String myEquals;
+    private String myComma;
+    
+    protected void parseLanguageFile(String fileName) {
+    	BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(fileName));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	String line;
+    	try {
+			while((line = reader.readLine()) != null) {
+				String spaceRemovedLine = line.replace("\\s+", "");
+				String[] splitLine = spaceRemovedLine.split(myEquals, 2);
+				String className = splitLine[0];
+				String[] possibleInputs = splitLine[1].split(myComma);
+				Class commandClass = Class.forName(className);
+				boolean extendsBaseCommand = BaseCommand.class.isAssignableFrom(commandClass);
+				if(extendsBaseCommand){
+					for(String possibleInput : possibleInputs){
+						myCommandToClassMap.put(possibleInput, commandClass);
+					}
+				}
+				else{
+					//TODO: Throw exception;
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO: Make Exception 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
     
     public static BaseCommand createCommand(String input){
         String firstCommand = identifyFirstCommand(input);
-        String inputCopy = new String(input);
         Class<BaseCommand> commandClass = myCommandToClassMap.get(firstCommand);
         if(commandClass == null){
             //throw something
         }
-        String subInput = inputCopy.replaceFirst(firstCommand, "");
+        String subInput = input.replaceFirst(firstCommand + myCommandSeparator, "");
+        BaseCommand command = null;
         try {
-            BaseCommand command = (BaseCommand) commandClass.getConstructor(String.class).newInstance(subInput);
+           command = (BaseCommand) commandClass.getConstructor(String.class).newInstance(subInput);
         }
-        catch (InstantiationException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        catch(Exception ex){
+        	//TODO: Change to more specific exceptions;
         }
-        catch (IllegalAccessException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (IllegalArgumentException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (InvocationTargetException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (NoSuchMethodException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (SecurityException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return null;
-        
-        
+        //TODO: Throw exception
+        return command;
+    }
+    
+    //TODO: remove if unnecessary
+    private void handleError(Exception ex){
+    	
     }
     
     private static String identifyFirstCommand(String input){
