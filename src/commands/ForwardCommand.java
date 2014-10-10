2149a333597ef1505ec2e4fdd722353d36744c60
Rahul Harikrishnan <rh151@duke.edu>
2014-10-09 20:27:07 -0400

    Forward command template

---
+++ src/commands/ForwardCommand.java
@@ -0,0 +1,12 @@
+package commands;
+
+public class ForwardCommand extends TurtleCommand {
+
+	public ForwardCommand(String userInput, boolean isExpression) {
+		super(userInput, isExpression);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
