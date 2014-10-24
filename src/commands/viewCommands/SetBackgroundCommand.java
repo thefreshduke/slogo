028ee6d0279b4bd19cd83c1db68480de24a24ae7
Rahul Harikrishnan <rh151@duke.edu>
2014-10-24 09:40:51 -0400

    Updating ViewCommand class and filling in setbg, setpc, setshape cmds

--- src/commands/viewCommands/SetBackgroundCommand.java
+++ src/commands/viewCommands/SetBackgroundCommand.java
@@ -1,5 +1,30 @@
 package commands.viewCommands;
 
-public class SetBackgroundCommand {
+import backendExceptions.BackendException;
+import commands.ViewCommand;
+import commands.information.BaseGridContainer;
+
+public class SetBackgroundCommand extends ViewCommand {
+
+	//TODO: Add the GUI function to call
+	private static final String SET_BACKGROUND_IMAGE = "";
+
+	public SetBackgroundCommand(String command, boolean isExpression)
+			throws BackendException {
+		super(command, isExpression);
+	}
+
+	@Override
+	protected double onExecute() throws BackendException {
+		BaseGridContainer grid = getGridContainer();
+		double bgIndex = getExpressionList()[0].execute();
+		grid.updateDisplayOptions(SET_BACKGROUND_IMAGE, bgIndex);
+		return bgIndex;
+	}
+
+	@Override
+	protected int getArgumentCount() {
+		return 1;
+	}
 
 }
