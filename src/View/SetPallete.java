40bf8f4a498ba716dfbc9033ff9fe30097ae1fb9
Petra Ronald <pkr8@duke.edu>
2014-10-25 15:37:22 -0400

    set palette

---
+++ src/View/SetPallete.java
@@ -0,0 +1,24 @@
+package View;
+
+import java.util.List;
+
+import GUIFunctions.GridFunction;
+
+public class SetPallete extends GridFunction{
+	ColorSelection myColors;
+	public SetPallete(ColorSelection colors){
+		myColors=colors;
+	}
+
+
+	@Override
+	public void doAction() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doAction(List<Number> newVal) {
+		
+
+	}
