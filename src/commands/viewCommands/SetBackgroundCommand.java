e29adf607ae00b7017961706f5588d9af204e3f6
Rahul Harikrishnan <rh151@duke.edu>
2014-10-26 00:50:42 -0400

    Set background change

--- src/commands/viewCommands/SetBackgroundCommand.java
+++ src/commands/viewCommands/SetBackgroundCommand.java
@@ -1,14 +1,16 @@
 package commands.viewCommands;
 
-import backendExceptions.BackendException;
+import java.util.ArrayList;
+import java.util.List;
 
+import backendExceptions.BackendException;
 import commands.ViewCommand;
 import commands.information.BaseGridContainer;
 
 public class SetBackgroundCommand extends ViewCommand {
 
     // TODO: Add the GUI function to call
-    private static final String SET_BACKGROUND_IMAGE = "";
+    private static final String SET_BACKGROUND_IMAGE = "backgroundColor";
 
     public SetBackgroundCommand (String command, boolean isExpression) throws BackendException {
         super(command, isExpression);
@@ -18,7 +20,9 @@ public class SetBackgroundCommand extends ViewCommand {
     protected double onExecute () throws BackendException {
         BaseGridContainer grid = getGridContainer();
         double bgIndex = getExpressionList()[0].execute();
-        grid.updateDisplayOptions(SET_BACKGROUND_IMAGE, bgIndex);
+        List<Double> bgIndexList = new ArrayList<Double>();
+        bgIndexList.add(bgIndex);
+        grid.updateDisplayOptions(SET_BACKGROUND_IMAGE, bgIndexList);
         return bgIndex;
     }
 
