d999eb9840afbfeef951f201d3da349d28c4d1db
Rahul Harikrishnan <rh151@duke.edu>
2014-10-26 00:19:52 -0400

    HomeCommand mostly working

--- src/commands/viewCommands/HomeCommand.java
+++ src/commands/viewCommands/HomeCommand.java
@@ -3,16 +3,18 @@ package commands.viewCommands;
 import java.util.ArrayList;
 import java.util.List;
 
+import turtle.Turtle;
 import View.Grid;
 import backendExceptions.BackendException;
+import commands.TurtleQuery;
+import commands.ViewQuery;
 import commands.information.BaseGridContainer;
 import commands.information.BaseTurtleContainer;
 import commands.turtleCommands.TurtleCommand;
 
-public class HomeCommand extends TurtleCommand {
+public class HomeCommand extends TurtleQuery {
 
     private static final String EXACTLY_ONE_ACTIVE_GRID_IS_NOT_SET = "Exactly one active grid is not set";
-	private static final String CLEAR_GUI = "";
 
     public HomeCommand (String command, boolean isExpression) throws BackendException {
         super(command, isExpression);
@@ -21,18 +23,14 @@ public class HomeCommand extends TurtleCommand {
     @Override
     protected double onExecute () throws BackendException {
         BaseGridContainer grid = getGridContainer();
-        grid.updateDisplayOptions(CLEAR_GUI);
         BaseTurtleContainer turtle = getTurtleContainer();
         List<Grid> gridList = (ArrayList<Grid>) grid.getActiveGrids();
         if (gridList.size() != 1) {
         	throw new BackendException(null, EXACTLY_ONE_ACTIVE_GRID_IS_NOT_SET);
         }
-        double distanceTraveled = turtle.setPosition(0, 0);
+        Grid activeGrid = gridList.get(0);
+        double distanceTraveled = turtle.setPosition(activeGrid.getWidth()/2, activeGrid.getHeight()/2);
+        grid.update(turtle.getActiveTurtles());
         return distanceTraveled;
-    }
-
-    @Override
-    protected int getArgumentCount () {
-        return 0;
     }
 }
