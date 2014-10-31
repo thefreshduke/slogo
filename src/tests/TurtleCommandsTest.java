70c0589af64e97b0d31f6594dad178d4b0123ed3
Rahul Harikrishnan <rh151@duke.edu>
2014-10-31 03:22:24 -0400

    Reformatted JUnit test/finalized masterpiece

--- src/tests/TurtleCommandsTest.java
+++ src/tests/TurtleCommandsTest.java
@@ -26,88 +26,86 @@ import commands.information.SingleGridInformationGateway;
  */
 public class TurtleCommandsTest {
 
-    @Test
-    public void testToParsing () {
-        setFactory();
-        String input = "FD SUM 75 25";
-        String input2 = "FD DIFFERENCE 125 50";
-        String input3 = "FD IFELSE SUM 50 50 [ RT 90 ] [ RT 25 ]";
-        String input4 = "FD IFELSE SUM 0 0 [ RT 90 ] [ RT 25 ]";
-        String input5 = "FD IF DIFFERENCE 15 2 [ FD SUM 25 FD SUM 12 13 ]";
-        String input6 = "FD GOTO 400 0";
-        String input7 = "SETXY 150 250";
-        String input8 = "TOWARDS 0 0";
-        
-        
+	@Test
+	public void testToParsing () {
+		setFactory();
+		String input = "FD SUM 75 25";
+		String input2 = "FD DIFFERENCE 125 50";
+		String input3 = "FD IFELSE SUM 50 50 [ RT 90 ] [ RT 25 ]";
+		String input4 = "FD IFELSE SUM 0 0 [ RT 90 ] [ RT 25 ]";
+		String input5 = "FD IF DIFFERENCE 15 2 [ FD SUM 25 FD SUM 12 13 ]";
+		String input6 = "FD GOTO 400 0";
+		String input7 = "SETXY 150 250";
+		String input8 = "TOWARDS 0 0";
 
-        try {
-            String processedCommand = processInput(input);
-            BaseCommand command = CommandFactory.createCommand(processedCommand, false);
-            Double result = command.execute();
-            assertEquals(new Double(100), result); 
+		try {
+			String processedCommand = processInput(input);
+			BaseCommand command = CommandFactory.createCommand(processedCommand, false);
+			Double result = command.execute();
+			assertEquals(new Double(100), result); 
 
-            processedCommand = processInput(input2);
-            command = CommandFactory.createCommand(processedCommand, false);
-            result = command.execute();
-            assertEquals(new Double(75), result); 
+			processedCommand = processInput(input2);
+			command = CommandFactory.createCommand(processedCommand, false);
+			result = command.execute();
+			assertEquals(new Double(75), result); 
 
-            processedCommand = processInput(input3);
-            command = CommandFactory.createCommand(processedCommand, false);
-            result = command.execute();
-            assertEquals(new Double(90), result); 
-            
-            processedCommand = processInput(input4);
-            command = CommandFactory.createCommand(processedCommand, false);
-            result = command.execute();
-            assertEquals(new Double(25), result);
-            
-            processedCommand = processInput(input5);
-            command = CommandFactory.createCommand(processedCommand, false);
-            result = command.execute();
-            assertEquals(new Double(50), result);
-            
-            processedCommand = processInput(input6);
-            command = CommandFactory.createCommand(processedCommand, false);
-            result = command.execute();
-            assertEquals(new Double(353.553390593), result);
-            
-            processedCommand = processInput(input7);
-            command = CommandFactory.createCommand(processedCommand, false);
-            result = command.execute();
-            assertEquals(new Double(250), result);
-            
-            processedCommand = processInput(input8);
-            command = CommandFactory.createCommand(processedCommand, false);
-            result = command.execute();
-            assertEquals(new Double(0), result);
-            
-        }
-        catch (BackendException ex) {
+			processedCommand = processInput(input3);
+			command = CommandFactory.createCommand(processedCommand, false);
+			result = command.execute();
+			assertEquals(new Double(90), result); 
 
-        }
-    }
+			processedCommand = processInput(input4);
+			command = CommandFactory.createCommand(processedCommand, false);
+			result = command.execute();
+			assertEquals(new Double(25), result);
 
-    private void setFactory () {
-        Grid grid = new SingleGrid();
-        Turtle turtle = new Turtle(new Position(0, 0), null);
-        IInformationGateway hub = new SingleGridInformationGateway(grid, turtle);
-        CommandFactory.setInformationGateway(hub);
-    }
+			processedCommand = processInput(input5);
+			command = CommandFactory.createCommand(processedCommand, false);
+			result = command.execute();
+			assertEquals(new Double(50), result);
 
-    private String processInput (String input) throws BackendException {
+			processedCommand = processInput(input6);
+			command = CommandFactory.createCommand(processedCommand, false);
+			result = command.execute();
+			assertEquals(new Double(353.553390593), result);
 
-        LanguageFileParser myTranslator =
-                new LanguageFileParser(new File("src/resources/languages/English.properties"));
-        CommandToClassTranslator commandToClassTranslator = new CommandToClassTranslator();
-        try {
-            CommandFactory.setCommandToClassRelation(
-                    commandToClassTranslator.translateCommandToClass(
-                            new File("src/resources/languages/EnglishToClassName.properties")));
-        }
-        catch (BackendException ex) {
-        	System.out.println("Could not set command to class relation");
-        }
-        return myTranslator.translateUserInputIntoEnglish(input);
+			processedCommand = processInput(input7);
+			command = CommandFactory.createCommand(processedCommand, false);
+			result = command.execute();
+			assertEquals(new Double(250), result);
 
-    }
+			processedCommand = processInput(input8);
+			command = CommandFactory.createCommand(processedCommand, false);
+			result = command.execute();
+			assertEquals(new Double(0), result);
+
+		}
+		catch (BackendException ex) {
+
+		}
+	}
+
+	private void setFactory () {
+		Grid grid = new SingleGrid();
+		Turtle turtle = new Turtle(new Position(0, 0), null);
+		IInformationGateway hub = new SingleGridInformationGateway(grid, turtle);
+		CommandFactory.setInformationGateway(hub);
+	}
+
+	private String processInput (String input) throws BackendException {
+
+		LanguageFileParser myTranslator =
+				new LanguageFileParser(new File("src/resources/languages/English.properties"));
+		CommandToClassTranslator commandToClassTranslator = new CommandToClassTranslator();
+		try {
+			CommandFactory.setCommandToClassRelation(
+					commandToClassTranslator.translateCommandToClass(
+							new File("src/resources/languages/EnglishToClassName.properties")));
+		}
+		catch (BackendException ex) {
+			System.out.println("Could not set command to class relation");
+		}
+		return myTranslator.translateUserInputIntoEnglish(input);
+
+	}
 }
